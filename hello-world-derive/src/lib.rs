3541e486a561a53c51f76d4c0b492c66a5a23d5f
Tyler Thrailkill <tyler.thrailkill@promontech.com>
2018-02-27 21:03:34 -0700

    Non-working example with new syn and quote version

--- hello-world-derive/src/lib.rs
+++ hello-world-derive/src/lib.rs
@@ -7,17 +7,14 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(HelloWorld)]
 pub fn hello_world(input: TokenStream) -> TokenStream {
-    // Construct a string representation of the type definition
-    let s = input.to_string();
-
     // Parse the string representation
-    let ast = syn::parse_derive_input(&s).unwrap();
+    let ast = syn::parse(input).unwrap();
 
     // Build the impl
     let gen = impl_hello_world(&ast);
 
     // Return the generated impl
-    gen.parse().unwrap()
+    gen.into()
 }
 
 fn impl_hello_world(ast: &syn::DeriveInput) -> quote::Tokens {
